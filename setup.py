8c4a9035615d6af633c4525b3525b0d27e2b2978
Ben Ghaem <ben.ghaem@gmail.com>
2017-06-01 21:28:26 -0400

    restructure into python lib

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='neonforest',
+    version='0.0.1',
+    description='A library for creating multi-agent simulations',
+    author='Benjamin Ghaemmaghami',
+    author_email='ben.ghaem@gmail.com',
+    url='https://github.com/benghaem/neonforest',
+    packages=['neonforest'],
+)
